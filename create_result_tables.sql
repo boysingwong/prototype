d7e6d6c36da0cf7b39b4e1283ce2845e7000f8f5
boysingwong <boysingwong@gmail.com>
2015-06-19 18:00:40 +0800

    First version of keyword search
    - include create table scripts
    - include script to trigger inserts

---
+++ create_result_tables.sql
@@ -0,0 +1,62 @@
+-- create temp table
+use scin_db;
+create table pub_keyword_temp
+(
+  doc_id      INT(11),
+  source      VARCHAR(20),         
+  source_id   INT(11),
+  keyword     VARCHAR(50),
+  content     LONGTEXT
+);
+
+create table pub_keyword_results
+(
+  id          INT(11)  NOT NULL AUTO_INCREMENT,
+  doc_id      INT(11),
+  source      VARCHAR(20),         
+  source_id   INT(11),
+  keyword     VARCHAR(50),
+  content     LONGTEXT,
+  sentence    VARCHAR(2000),
+  PRIMARY KEY (id)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+drop table pub_keyword_result_splits;
+create table pub_keyword_result_splits
+(
+  id          INT(11) NOT NULL AUTO_INCREMENT,
+  rslt_id     INT(11) NOT NULL,
+  sentence    VARCHAR(2000),
+  word        VARCHAR(20),
+  PRIMARY KEY (id),
+  CONSTRAINT `pub_keyword_result_splt_fk_1` FOREIGN KEY (`rslt_id`) REFERENCES `pub_keyword_results` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+
+-- insert result into target  (generalize it into stored procedure later)
+SELECT id, 'TITLE', id, 'Huridin', title  FROM scin_pub_meta WHERE title COLLATE UTF8_GENERAL_CI LIKE '%Huridin%';
+SELECT doc_id, 'DISCUSSION', id, 'Huridin', content FROM scin_pub_discussion WHERE content COLLATE UTF8_GENERAL_CI LIKE '%Huridin%';
+SELECT doc_id, 'ABSTRACT', id, 'Huridin', content FROM scin_pub_abstract  WHERE content COLLATE UTF8_GENERAL_CI LIKE '%Huridin%';
+
+
+/* python scripts 
+1. write a script to further process content and extract into sentence (reference to current scripts)
+2. write a script to split sentence into keywords
+*/
+
+-- test case
+INSERT INTO pub_keyword_temp (doc_id, source, source_id, keyword, content)
+VALUES (1, 'TITLE', 1, 'test', 'This is a test case');
+INSERT INTO pub_keyword_temp (doc_id, source, source_id, keyword, content)
+VALUES (2, 'TITLE', 2, 'TEST', 'This is a test case. This is test case2.');
+
+select * from pub_keyword_temp;
+select * from pub_keyword_results;
+select * from pub_keyword_result_splits;
+truncate table pub_keyword_temp;
+truncate table pub_keyword_result_splits;
+delete from pub_keyword_results;
+alter table pub_keyword_results AUTO_INCREMENT = 1;
+
+
+SELECT id as rslt_id FROM scin_db.pub_keyword_temp WHERE doc_id = 1 AND source_id = 1;
---
+++ search_keyword.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*- 
+import codecs
+import MySQLdb
+import re
+import sys
+from warnings import filterwarnings
+
+def search_keyword():
+	filterwarnings('ignore', category = MySQLdb.Warning)
+	sys.stdout = codecs.getwriter('utf8')(sys.stdout)
+
+	print "start connection"
+	try:
+		# mysql = MySQLdb.connect(user='root',passwd='password1',db='scin_db',host='127.0.0.1',port=3306, autocommit = 'True', charset = 'utf8', use_unicode = True)
+		mysql = MySQLdb.connect(user='root',passwd='db_4tp3fen',db='scin_db',host='127.0.0.1',port=8796, autocommit = 'True', charset = 'utf8', use_unicode = True)
+		mysql_cursor = mysql.cursor()
+		print "connect successfully"
+		
+		# extract result from result tables
+		# TODO: call procedure to search and insert search result
+		
+		query = ("SELECT doc_id, source, source_id, keyword, content FROM scin_db.pub_keyword_temp")
+		mysql_cursor.execute(query)
+		
+		rsltCount = 0
+		for (doc_id, source, source_id, keyword, content) in mysql_cursor:
+			sentenceList = re.split(ur'(?<!\w\.\w.)(?<![A-Z]\.)(?<=\.|\?)\s', content)
+			for sentence in sentenceList:
+				keyword_exists = False
+
+				print "process sentence [%s]" % sentence
+
+				# a. check keyword exists
+				pattern = ur'(?i)\b%s\b' % (keyword)
+				if re.search(pattern, sentence):
+					print "keyword found [%s]" % keyword
+					keyword_exists = True
+				else:
+					continue
+
+				if keyword_exists:
+					print "insert result to result_table"
+					insertStmt = ("INSERT INTO scin_db.pub_keyword_results "
+								  "(doc_id, source, source_id, keyword, content, sentence) "
+								  "VALUES (%s, %s, %s, %s, %s, %s)")
+					mysql_cursor.execute(insertStmt, (doc_id, source, source_id, keyword, content, sentence) )
+					mysql.commit()
+
+					rsltCount = rsltCount + 1
+
+
+		# STEP 2: insert into word table
+		# get result Id
+		queryInner = ("SELECT id, sentence FROM scin_db.pub_keyword_results")
+		mysql_cursor.execute( queryInner )
+
+		for (id, sentence) in mysql_cursor:
+			sentence = sentence.rstrip('.')
+			wordList = re.split('\s', sentence)
+			for word in wordList:
+				insertStmt2 = ("INSERT INTO scin_db.pub_keyword_result_splits "
+							   "(rslt_id, sentence, word) "
+							   "VALUES (%s, %s, %s)")
+				mysql_cursor.execute(insertStmt2, (id, sentence, word))
+				mysql.commit()
+
+		mysql_cursor.close()
+		mysql.close()
+		
+		return rsltCount
+
+	except MySQLdb.Error, e:
+		errmsg = "MySQL Error (@%s) %d:  %s" % ( keyword, e.args[0], e.args[1] )
+		with open("error.log", 'w') as w:
+			w.write(errmsg)
+		sys.exit(1)
+
+def main():
+	search_keyword()
+
+if __name__ == '__main__': main()
