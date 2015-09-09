6fced4700d297bbeec1deabe56835f7b126eea81
boysingwong <boysingwong@gmail.com>
2015-09-09 17:46:54 +0800

    keyword indexing

--- split_words/keyword_indexing.py
+++ split_words/keyword_indexing.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 import re
 import codecs
+import itertools
 
 # arguement setup
-input_filename = "Abgent_AMAP_input.txt"
-output_filename = "Abgent_AMAP_output.txt"
-table_name = "temp_Abgent_AMAP_indexing"
+input_filename = "Abgent_AMAP_B_input.txt"
+output_filename = "Abgent_AMAP_B_output.txt"
+table_name = "Abgent_AMAP_B_indexing"
 
 delimiters = "-", " ", '/', "(", ")", "[", "]"
 regexPattern = "|".join(map(re.escape, delimiters))
@@ -16,22 +17,47 @@ outputFile = codecs.open(output_filename, encoding='utf-8', mode='w+')
 with codecs.open(input_filename) as f:
     lines = f.read().splitlines()
     for line in lines:
-        # replace greek alphabet
+        print line
+
+        # change line encoding as utf-8
         line = line.decode('utf-8')
 
-        line = line.replace(u"α", "a")
-        line = line.replace(u"β", "b")
-        line = line.replace(u"γ", "g")
-        line = line.replace(u"δ", "d")
-        line = line.replace(u"ε", "e")
-        line = line.replace(u"ζ", "z")
-        line = line.replace(u"θ", "t")
-        line = line.replace(u"κ", "k")
-        line = line.replace(u"λ", "l")
-        line = line.replace(u"Σ", "s")
+        # split catalog_nb,product_fullname
+        lineItems = re.split(u",", line, flags=re.UNICODE)
+        catalogNb = lineItems[0]
+        prodName = lineItems[1]
 
-        list = re.split(regexPattern, line)
-        #outputStr = line
+        # replace greek alphabet
+        prodName = prodName.replace(u"α", "a")
+        prodName = prodName.replace(u"β", "b")
+        prodName = prodName.replace(u"γ", "g")
+        prodName = prodName.replace(u"δ", "d")
+        prodName = prodName.replace(u"ε", "e")
+        prodName = prodName.replace(u"ζ", "z")
+        prodName = prodName.replace(u"θ", "t")
+        prodName = prodName.replace(u"κ", "k")
+        prodName = prodName.replace(u"λ", "l")
+        prodName = prodName.replace(u"Σ", "s")
+
+        listItems = re.split(regexPattern, prodName)
+        listItems = filter(lambda a: a != "", listItems)
+        if "" in listItems:
+            listItems.remove("")
+
+        # combinations of synonyms
+        synCombList = []
+        for i in range(1, len(listItems)+1):
+            tempList = list(itertools.combinations(listItems, i))
+            synCombList.extend(tempList)
+
+        outputSynonyms = []
+        for synCombItem in synCombList:
+            output1 = " ".join(synCombItem)
+            output2 = "-".join(synCombItem)
+            if len(output1) >= 3:
+                outputSynonyms.append(output1)
+            if len(output2) >= 3 and output1 <> output2:
+                outputSynonyms.append(output2)
 
 		# insert concatenated
         # spaceJointStr = "".join(list)
@@ -50,10 +76,9 @@ with codecs.open(input_filename) as f:
         #     outputStr += "-".join(list)
 
         outputStr = ""
-        for item in list:
-            if len(item) > 0:
-                outputStr += "insert into " + table_name + " values ('" + item + "');"
-                outputStr += '\n'
+        for item in outputSynonyms:
+            outputStr += catalogNb + "," + item
+            outputStr += '\n'
 
         outputFile.write(outputStr)
 
